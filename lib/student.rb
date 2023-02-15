53aa528bad02ef184dd5717dec6fc3b1f3de5daf
brianonyango <brian.ogango@student.moringaschool.com>
2023-02-15 15:50:22 +0300

    complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+#loads the code from user so that we can use it here
+require_relative './user'
+class Student < User
+     attr_accessor :knowledge
+def initialize
+    @knowledge = []
+def
+    def learn(string)
+        @knowledge << string
 
+    end
+    def knowledge
+        @knowledge
+    end
 end
